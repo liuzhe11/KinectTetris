91e1bfb15ffb81a8767568ea6caf58b58811e8e8
liuzhe11 <liuzhehelen@163.com>
2014-08-08 22:50:50 +0800

    Bug fix

--- 程序（updating）/Kinect TetrisV2/Block.cs
+++ 程序（updating）/Kinect TetrisV2/Block.cs
@@ -109,12 +109,12 @@ namespace Kinect_TetrisV2
             Pen p = new Pen(Color.FromArgb(200,200,200), 1);
             for (int y = 0; y < yCellCnt; ++y)
             {
-                grMain.DrawLine(p, 0, y * cellSize, xCellCnt * cellSize, y * cellSize);
+                g.DrawLine(p, 0, y * cellSize, xCellCnt * cellSize, y * cellSize);
             }
 
             for (int x = 0; x < xCellCnt; ++x)
             {
-                grMain.DrawLine(p, x * cellSize, 0, x * cellSize, yCellCnt * cellSize);
+                g.DrawLine(p, x * cellSize, 0, x * cellSize, yCellCnt * cellSize);
             }
         }
 
