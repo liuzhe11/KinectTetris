7b5bae6545c8e5c4d76b186691bde25d1a44a7fe
Wu Peng <wupengjiexin@gmail.com>
2014-08-19 23:08:36 +0800

    Try TransPanel

---
+++ 程序（updating）/Kinect TetrisV2/TransPanel.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+
+
+namespace Kinect_TetrisV2
+{
+  /// <summary>
+  /// Summary description for TransPanel.
+  /// </summary>
+  public class TransPanel : Panel
+  {
+    private Skeleton skeleton;
+    public TransPanel()
+    {
+      //
+      // TODO: Add constructor logic here
+      //
+    }
+
+    public void SetSkeleton(newSkeleton) {
+      this.skeleton = newSkeleton;
+    }
+
+    protected override CreateParams CreateParams
+    {
+      get
+      {
+        CreateParams cp=base.CreateParams;
+        cp.ExStyle|=0x00000020; //WS_EX_TRANSPARENT
+        return cp;
+      }
+    }
+
+    protected void InvalidateEx()
+    {
+      if(Parent==null)
+        return;
+
+      Rectangle rc=new Rectangle(this.Location,this.Size);
+      Parent.Invalidate(rc,true);
+    }
+
+    protected override void OnPaintBackground(PaintEventArgs pevent)
+    {
+      //do not allow the background to be painted
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+      if (this.skeleton != null) {
+        DrawBonesAndJoints(this.skeleton, e.Graphics)
+      }
+    }
+
+
+
+  }
+}
